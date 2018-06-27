2c73a1d98270be31018bd58c01e531064533e031
jamos90 <jamos_98@hotmail.com>
2018-06-27 11:27:55 +0100

    player tests passed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Player {
+    
+    private String name;
+    private ArrayList<Card> hand;
+    
+    public Player(String name){
+        this.hand = new ArrayList<>();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int handCount() {
+        return this.hand.size();
+    }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlayerTest {
+
+    Player player;
+
+    @Before
+    public void setUp(){
+        player = new Player("Adam");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Adam", player.getName());
+    }
+
+    @Test
+    public void handStartsEmpty(){
+        assertEquals(0,player.handCount());
+    }
 }
